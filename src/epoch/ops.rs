30304696f0103922d97a4aafab8381c31d469bad
Roman <roman.lemontagner@gmail.com>
2025-08-28 10:36:32 +0200

    fix clippy for partial_cmp

--- src/epoch/ops.rs
+++ src/epoch/ops.rs
@@ -495,9 +495,7 @@ impl PartialEq for Epoch {
 
 impl PartialOrd for Epoch {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        {
-            Some(self.cmp(other))
-        }
+        Some(self.cmp(other))
     }
 }
 
