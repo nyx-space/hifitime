37aee7f92793cdee21ef6346f4c0308a01ad8535
Johan Westin <johan@aikospace.com>
2025-09-30 16:03:52 +0000

    remove redundant equality check for duration max as suggested by gemini

--- src/duration/mod.rs
+++ src/duration/mod.rs
@@ -336,7 +336,7 @@ impl Duration {
                     normalized_self = Self::MAX;
                 }
                 // Else, we're near the MAX but we're within the MAX in nanoseconds, so let's not do anything here.
-            } else if !self.parts_are_equal(Self::MAX) && !self.parts_are_equal(Self::MIN) {
+            } else if !self.parts_are_equal(Self::MIN) {
                 // The bounds are valid as is, no wrapping needed when rem_nanos is not zero.
                 match self.centuries.checked_add(extra_centuries as i16) {
                     Some(centuries) => {
